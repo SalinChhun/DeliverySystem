466749a51934fa2651c31c87836b246f0178537a
SalinChhun <salinchhun123079@mail.com>
2024-01-30 18:28:34 +0700

    first init

---
+++ routes/web.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\Http\Controllers\AboutController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\homeController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\ContactController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/sample', function () {
+    return "wecome to salin chhun laravel";
+});
+
+Route::redirect('/here', 'there');
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::get('home', [homeController::class, 'index']);
+
+Route::get('contact', [ContactController::class, 'index']);
+
+Route::get('about', [AboutController::class, 'index']);
+
+Route::get('register/save', [RegisterController ::class,'save']);
+
+
+// Route::controllers(RegisterController::class)->group(function(){
+//     Route::view('register');
+// });
